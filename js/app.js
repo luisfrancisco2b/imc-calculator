2f278212acb327d309ef37b90120ec2abd4ed7f2
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-07-28 22:18:03 -0300

    feat(js): add IMC data

--- js/app.js
+++ js/app.js
@@ -1,47 +1,38 @@
-// Selecting Elements
-const height = document.querySelector("#height");
-const weight = document.querySelector("#weight");
-
-const CalcBtn = document.querySelector("#calc-btn");
-
-// Funtions
-const calcImc = (height, weight) => {
-  // Imc Calc
-  const imc = weight / (height * height);
-
-  //   Underweight Verification
-  if (imc < 18.5) {
-    console.log(`Seu IMC é: ${imc.toFixed(1)}. Sua situação: Abaixo do peso.`);
-  }
-
-  //   Normal weight Verification
-  if ((imc >= 18.5 && imc < 24.9)) {
-    console.log(
-      `Seu IMC é: ${imc.toFixed(1)}. Sua situação: Peso normal (saúdavel).`,
-    );
-  }
-
-  //   Overweight Verification
-  if ((imc >= 25 && imc < 29.9)) {
-    console.log(
-      `Seu IMC é: ${imc.toFixed(1)}. Sua situação: Sobrepeso (peso).`,
-    );
-  }
-
-  //   Obesity Verification
-  if (imc >= 30) {
-    console.log(`Seu IMC é: ${imc.toFixed(1)}. Sua situação: Obesidade.`);
-  }
-};
-
-// Events
-CalcBtn.addEventListener("click", () => {
-  // Get height value
-  const heightValue = parseFloat(height.value).toFixed(1);
-
-  //   Get weight value
-  const weightValue = parseFloat(weight.value).toFixed(1);
-
-  // Call of the imc calc function
-  calcImc(heightValue, weightValue);
-});
+// IMC DATA
+const data = [
+  {
+    min: 0,
+    max: 18.4,
+    classification: "Menor que 18,5",
+    info: "Magreza",
+    obesity: "0",
+  },
+  {
+    min: 18.5,
+    max: 24.9,
+    classification: "Entre 18,5 e 24,9",
+    info: "Normal",
+    obesity: "0",
+  },
+  {
+    min: 25,
+    max: 29.9,
+    classification: "Entre 25,0 e 29,9",
+    info: "Sobrepeso",
+    obesity: "I",
+  },
+  {
+    min: 30,
+    max: 39.9,
+    classification: "Entre 30,0 e 39,9",
+    info: "Obesidade",
+    obesity: "II",
+  },
+  {
+    min: 40,
+    max: 99,
+    classification: "Maior que 40,0",
+    info: "Obesidade grave",
+    obesity: "III",
+  },
+];
