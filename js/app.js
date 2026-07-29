9e9600ed44243d3ff8180a9442b276499f21ad71
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-07-28 21:15:13 -0300

    feat(js): add initial js code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("Olá!");
+// Selecting Elements
+const height = document.querySelector("#height");
+const weight = document.querySelector("#weight");
+const CalcBtn = document.querySelector("#calc-btn");
+
+// Funtions
+const calcImc = (height, weight) => {
+  console.log(height, weight);
+};
+
+// Events
+CalcBtn.addEventListener("click", () => {
+  const heightValue = parseFloat(height.value).toFixed(1);
+  const weightValue = parseFloat(weight.value).toFixed(1);
+
+  calcImc(heightValue, weightValue);
+});
